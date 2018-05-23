62763e045c4429c84fe70d8501f1259c58af30a9
Stefan Bächler <stefan.baechler@nexum.ch>
2018-05-23 14:30:48 +0200

    fixed typos, added colored output

--- src/SteBach/FixedVersions/CommandProvider.php
+++ src/SteBach/FixedVersions/CommandProvider.php
@@ -8,7 +8,7 @@ use Composer\Command\BaseCommand;
 
 class CommandProvider implements CommandProviderCapability
 {
-	public static $composer;
+    public static $composer;
 
     public function getCommands()
     {
@@ -18,30 +18,30 @@ class CommandProvider implements CommandProviderCapability
 
 class Command extends BaseCommand
 {
-	private $composer;
-	private $localPackages = [];
-	private $rawData = [];
+    private $composer;
+    private $localPackages = [];
+    private $rawData = [];
 
-	public function __construct($composer) {
-		parent::__construct();
-		$this->composer = $composer;
-	}
+    public function __construct($composer) {
+        parent::__construct();
+        $this->composer = $composer;
+    }
 
     protected function configure()
     {
         $this->setName('fixversions');
-        $this->setDescription("set fixed versions of your dependencies");
+        $this->setDescription("Set fixed versions of your dependencies");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 
-    	$repositoryManager = $this->composer->getRepositoryManager();
+        $repositoryManager = $this->composer->getRepositoryManager();
 
-    	$file = $this->composer->getConfig()->getConfigSource()->getName();
-    	$output->writeln("<fg=green>Checking ".$file."</>");
-    	$this->rawData = json_decode(file_get_contents($file),true);
-    	foreach ($repositoryManager->getRepositories() as $repository) {
+        $file = $this->composer->getConfig()->getConfigSource()->getName();
+        $output->writeln("<fg=green>Checking ".$file."</>");
+        $this->rawData = json_decode(file_get_contents($file),true);
+        foreach ($repositoryManager->getRepositories() as $repository) {
             $this->checkPackages(
                 $repositoryManager->getLocalRepository()
             );
@@ -49,21 +49,24 @@ class Command extends BaseCommand
 
         $checkKeys = ['require','require-dev'];
         foreach ($checkKeys as $checkkey) {
-        	if (isset($this->rawData[$checkkey])) {
-        		$output->writeln("- " . $checkkey);
-        		foreach ($this->rawData[$checkkey] as $name => $version) {
-    				if (isset($this->localPackages[$name])) {
-    					if ($version != $this->localPackages[$name]) {
-    						$this->rawData[$checkkey][$name] = $this->localPackages[$name];
-			        		$output->writeln("  - package \"" . $name . "\" was changed  to version \"" . $this->localPackages[$name] . "\"");
-    					} else {
-			        		$output->writeln("  - package \"" . $name . "\" was already set to version \"" . $version . "\"");
-    					}
-    				} else {
-		        		$output->writeln("  - package \"" . $name . "\" not found - version remains \"" . $version . "\"");
-    				}
-        		}
-        	}
+            if (isset($this->rawData[$checkkey])) {
+                $output->writeln("- " . $checkkey);
+                foreach ($this->rawData[$checkkey] as $name => $version) {
+                    if ($name == 'stebach/fixedversions') {
+                        $this->rawData[$checkkey][$name] = '^1.0';
+                    }
+                    if (isset($this->localPackages[$name])) {
+                        if ($version != $this->localPackages[$name]) {
+                            $this->rawData[$checkkey][$name] = $this->localPackages[$name];
+                            $output->writeln("  - package \"<fg=green>" . $name . "</>\" was changed  to version \"<fg=green>" . $this->localPackages[$name] . "</>\"");
+                        } else {
+                            $output->writeln("  - package \"<fg=yellow>" . $name . "</>\" was already set to version \"<fg=yellow>" . $version . "</>\"");
+                        }
+                    } else {
+                        $output->writeln("  - package \"<fg=orange>" . $name . "</>\" not found - version remains \"<fg=orange>" . $version . "</>\"");
+                    }
+                }
+            }
         }
         $output->writeln("");
         $output->writeln("Saving backup to " . $file . ".bak." . date("YmdHis") . " ...");
@@ -76,9 +79,9 @@ class Command extends BaseCommand
     }
 
     private function checkPackages($local) {
-    	foreach ($local->getPackages() as $package) {
-    		$this->localPackages[$package->getName()] = $package->getVersion();
-    	}
+        foreach ($local->getPackages() as $package) {
+            $this->localPackages[$package->getName()] = $package->getVersion();
+        }
     }
 
 
